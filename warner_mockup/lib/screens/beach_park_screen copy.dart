ae91a578db22e0f76e26e8f91331c53cc15c9106
JasonDGian <dgia543@g.educaand.es>
2024-11-19 08:52:20 +0100

    configuracion de assets pendiente

--- warner_mockup/lib/screens/beach_park_screen copy.dart
+++ warner_mockup/lib/screens/beach_park_screen copy.dart
@@ -6,14 +6,12 @@ class BeachParkScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    var size = MediaQuery.of(context).size;
-
     // Row de navegacion.
     var navigationRow = Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back)),
-              Image.asset("assets/img/logo.png"),
+              Image.network(""),
               IconButton(onPressed: (){}, icon: Icon(Icons.notifications_outlined))
             ],
           );
@@ -30,16 +28,11 @@ class BeachParkScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                decoration: const BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(25), bottomLeft: Radius.circular(25))),
+                decoration: decoracionBotones,
                 child: TextButton.icon(onPressed: (){}, label: const Text("Buscar", style: decoracionTextoBotones), icon: const Icon(Icons.search, color: Colors.white,),),
               ),
-              const SizedBox(width: 1,),
               Container(
-                decoration: const BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.only(topRight: Radius.circular(25), bottomRight: Radius.circular(25))),
+                decoration: decoracionBotones,
                 child: TextButton.icon(onPressed: (){}, label: const Text("Mis entradas", style: decoracionTextoBotones), icon: const Icon(Icons.ad_units_outlined, color: Colors.white,),),
               )
             ],
@@ -62,55 +55,59 @@ class BeachParkScreen extends StatelessWidget {
                   child: const Text("Mapa"),
                 );
 
-    _entradaMenu( icono, String texto, funcion ) {
+    _entradaMenu( icono, String texto ) {
         return 
-          ListTile(
-            leading: Icon(icono),
-            title: Text(texto),
-            trailing: const Icon(Icons.arrow_forward_ios),
-            onTap: funcion,
-          );
+        Row(
+        children: [
+          Icon(icono), 
+          Text(texto),
+          const Expanded(child: SizedBox()),
+          const Icon(Icons.arrow_forward_ios)
+        ],
+      );
     }
 
     var listaEntradasMenu = [
-      scsvRowAtraccionesRestaurantes,
-      scsvMapa,
-      _entradaMenu(Icons.storefront_outlined, "Tienda", (){}),
-      _entradaMenu(Icons.map_outlined, "Cómo llegar", (){}),
-      _entradaMenu(Icons.calendar_month_outlined, "Horario de apertura", (){}),
-      _entradaMenu(Icons.roller_shades_sharp, "Otros servicios", (){}),
-      _entradaMenu(Icons.sticky_note_2_sharp, "Normas de funcionamiento", (){}),
+      _entradaMenu(Icons.storefront_outlined, "Tienda"),
+      _entradaMenu(Icons.map_outlined, "Cómo llegar"),
+      _entradaMenu(Icons.calendar_month_outlined, "Horario de apertura"),
+      _entradaMenu(Icons.roller_shades_sharp, "Otros servicios"),
+      _entradaMenu(Icons.sticky_note_2_sharp, "Normas de funcionamiento"),
     ];
 
-    var decoration = const BoxDecoration(
-      borderRadius: BorderRadius.only(topLeft: Radius.circular(25), topRight: Radius.circular(25)),
-      color: Colors.white
-    );
-
     return Scaffold(
-      body: Container(
-        color: Colors.cyan,
+      body: Padding(
+        padding: const EdgeInsets.all(5.0),
         child: Column(
           children: [
             // Elementos fijos
-            navigationRow,
-            Container(
-              decoration: decoration,
-              padding: const EdgeInsets.all(10),
-              child: buttonRow),
+            Expanded(
+              flex: 1,
+              child: navigationRow,
+              ),
+            Expanded(
+              flex: 1,
+              child: buttonRow,
+              ),
             // Elementos deslizables.
             Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                color: Colors.white,
-                child:
-                ListView.builder(
-                  itemCount: listaEntradasMenu.length,
-                  itemBuilder:(context, index) {
-                    
-                    return listaEntradasMenu[index];
-                  } , 
-                ),
+              flex: 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  scsvRowAtraccionesRestaurantes,
+                  scsvMapa,
+                  Expanded(
+                    child:
+                    ListView.builder(
+                      itemCount: listaEntradasMenu.length,
+                      itemBuilder:(context, index) {
+                        return listaEntradasMenu[index];
+                      } , 
+                    ) 
+                  )
+                ],
               ),
             ),
           ],
